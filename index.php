5b40b7aa74f3ff69cf7d103089ff991492776ec4
kkishans <kishan7575t@gmail.com>
2018-07-03 22:38:08 +0530

    I add two files
    that is demo of theme and css page

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+    <title>Home</title>
+  </head>
+  <body>
+  	<nav class="navbar navbar-light" style="background-color: #e3f2fd;">
+     <div class="nav text-center">
+     	<div class="form-group">
+    <label for="mediumm">Select Medium</label>
+    <select class="form-control" id="medium">
+      
+      <option>Gujarati</option>
+      <option>English </option>
+      
+    </select>
+ </div>
+ <div class="form-group">
+    <label for="search">Enter Roll No.</label>
+    <input type="number" class=" main form-control" id="search-box" placeholder="roll no">
+  </div>
+     </div>
+     <div class="form-group">
+     	<button type="button" class="btn btn-outline-info "  data-target="#exampleModalCenter">
+  Find Result
+</button>
+
+     </div>
+</nav>
+
+  </body>
+</html
