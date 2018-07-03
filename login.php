683485dec7029e280ed2b4416ff5ac8cd5e9a112
nnikunjt <thakornikunj152@gmail.com>
2018-07-03 22:51:25 +0530

    2 files
    now dashboard is empty

---
+++ login.php
@@ -0,0 +1,53 @@
+<?php
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Complete Bootstrap 4 Website Layout</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+	<script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+	<link href="style.css" rel="stylesheet">
+</head>
+<body>
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-2"></div>
+		<div class="col-md-8">
+			<h1 class="card-title">Login</h1>
+			<div class="card">
+				<div class="card-body">
+					<form class="" action="login.php" method="post">
+						<div class="form-group row">
+		                    <label for="username" class="col-sm-2 col-form-label">User name</label>
+		                    <div class="col-sm-10">
+		                        <input type="text" class="form-control" name="username" value="" autofocus required>
+		                    </div>
+		                </div>
+						<div class="form-group row">
+		                    <label for="password" class="col-sm-2 col-form-label">Password</label>
+		                    <div class="col-sm-10">
+		                        <input type="text" class="form-control" name="password" value="" required>
+		                    </div>
+		                </div>
+						<input type="submit" class="btn btn-outline-primary btn-lg" name="login" value="Login">
+					</form>
+				</div>
+			</div>
+		</div>
+
+	</div>
+
+</div>
+</body>
+</html>
