bb59eef597a462556298c4c1c7adde30a4fb7245
kkishans <kishan7575t@gmail.com>
2018-07-04 21:50:21 +0530

    that i chage..
    ok

--- add_result.php
+++ add_result.php
@@ -10,7 +10,6 @@
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 	<script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
 	<link href="style.css" rel="stylesheet">
-<<<<<<< HEAD
 	<style media="screen">
 		.container{
 			padding-top: 1rem;
@@ -19,8 +18,6 @@
 			font-size: 20px;
 		}
 	</style>
-=======
->>>>>>> f11d45a8cc618635fb0b66371ddc74c93125ee39
 </head>
 <body>
 
@@ -29,22 +26,12 @@
 			<a href="#" class="navbar-brand">Yash classes</a>
 		</div>
 		<ul class="navbar-nav">
-<<<<<<< HEAD
-
-      <a href="index.php" type="button" class="btn btn-primary " >Log out</a>
-
-=======
-			
-      <a href="index.php" type="button" class="btn btn-primary " >Log out</a> 
-  
->>>>>>> f11d45a8cc618635fb0b66371ddc74c93125ee39
-
+      		<a href="index.php" type="button" class="btn btn-primary " >Log out</a>
 		</ul>
 	</nav>
 	<div class="container-fluid">
 		<div class="row">
 			<div class="col-md-3">
-<<<<<<< HEAD
 				<div class="container">
 					<div class="card">
 						<div class="card-header">
@@ -53,169 +40,94 @@
 						<div class="card-body">
 							<ul class="list-group">
 								<a href="add_student.php" class="list-group-item">Add student</a>
-								<a href="#" class="list-group-item disabled">Add result</a>
+								<a href="" class="list-group-item disabled">Add result</a>
 							</ul>
 						</div>
-=======
-				<div class="card">
-					<div class="card-header">
-						Links
-					</div>
-					<div class="card-body">
-						<ul class="list-group">
-							<a href="add_student.php" class="list-group-item">Add student</a>
-							<a href="#" class="list-group-item disabled">Add result</a>
-						</ul>
->>>>>>> f11d45a8cc618635fb0b66371ddc74c93125ee39
 					</div>
 				</div>
 			</div>
 			<div class="col-md-9">
 				<div class="container">
-<<<<<<< HEAD
-				    <div class="row">
-				        <div class="col-md-1"></div>
-				        <div class="col-md-10">
-				            <h1 class="card-title">Add Result</h1>
-				            <div class="card">
-				                <div class="card-body">
-				                    <form class="" action="index.html" method="post">
-				                        <div class="form-group row">
-				                            <label for="exam_date" class="col-sm-2 col-form-label">Test date</label>
-				                            <div class="col-md-4">
-				                                <input type="date" class="form-control" name="exam_date" value="" required autofocus>
-				                            </div>
-				                            <label for="medium" class="col-sm-2 col-form-label">Medium</label>
-				                            <div class="col-sm-4">
-				                                <select class="form-control" name="medium">
-				                                    <option>Gujarati</option>
-				                                    <option>English(GSEB)</option>
-				                                    <option>English(CBSC)</option>
-				                                </select>
-				                            </div>
-				                        </div>
-				                        <div class="form-group row">
-				                            <label for="std" class="col-sm-2 col-form-label">Std.</label>
-				                           <div class="col-sm-4">
-				                               <select class="form-control" name="std">
-				                                   <option>1</option>
-				                                   <option>2</option>
-				                                   <option>3</option>
-				                                   <option>4</option>
-				                                   <option>5</option>
-				                                   <option>6</option>
-				                                   <option>7</option>
-				                                   <option>8</option>
-				                                   <option>9</option>
-				                                   <option>10</option>
-				                               </select>
-				                           </div>
-				                        </div>
-				                        <div class="form-group row">
-				                            <label for="roll_no" class="col-sm-2 col-form-label">Roll no.</label>
-				                            <div class="col-sm-2">
-				                                <input type="text" name="roll_no" class="form-control" value="" required>
-				                            </div>
-				                        </div>
-				                        <div class="form-group row">
-				                            <label for="student_name" class="col-sm-2 col-form-label">Student name</label>
-				                            <div class="col-sm-6">
-				                                <input type="text" class="form-control" name="student_name" value="" required>
-				                            </div>
-				                        </div>
-				                        <div class="form-group row">
-				                            <label class="col-sm-2 col-form-label header">Marks :</label>
-				                        </div>
-				                        <div class="form-group row">
-				                            <label for="math" class="col-sm-2 col-form-label">Math</label>
-				                            <div class="col-sm-3">
-				                                <input type="text" name="math" class="form-control" value="">
-				                            </div>
-				                        </div>
-				                        <div class="form-group row">
-				                            <label for="science" class="col-sm-2 col-form-label">science</label>
-				                            <div class="col-sm-3">
-				                                <input type="text" name="science" class="form-control" value="">
-				                            </div>
-				                        </div>
-				                        <div class="form-group row">
-				                            <label for="ss" class="col-sm-2 col-form-label">Social science</label>
-				                            <div class="col-sm-3">
-				                                <input type="text" name="ss" class="form-control" value="">
-				                            </div>
-				                        </div>
-				                        <center><input type="submit" name="submit" value="Submit" class="btn btn-outline-primary btn-lg"></center>
-				                    </form>
-				                </div>
-				            </div>
-				        </div>
-				    </div>
-=======
 					<div class="row">
-						<div class="col-md-1"></div>
-						<div class="col-md-10">
-							<h1 class="card-title">Add Result</h1>
-						    <div class="card">
-						        <div class="card-body">
-						        	<div class="form-group row">
-						             <label for="medium" class="col-sm-2 col-form-label">Medium</label>
-						                 <div class="col-sm-4">
-						                        <select class="form-control" name="medium">
-						                            <option>Gujarati</option>
-						                            <option>English(GSEB)</option>
-						                            <option>English(CBSC)</option>
-						                        </select>
-						                    </div>
-						                    <div class="select form-group row" align="center">
-						                    <label for="std" class="col-form-label">Std.</label>
-						                    <div class="text-center">
-						                        <select class="form-control" name="std">
-						                            <option>1</option>
-						                            <option>2</option>
-						                            <option>3</option>
-						                            <option>4</option>
-						                            <option>5</option>
-						                            <option>6</option>
-						                            <option>7</option>
-						                            <option>8</option>
-						                            <option>9</option>
-						                            <option>10</option>
-						                        </select>
-						                    </div>
-						                    </div>
-						                </div>
-						            <div class="form-group col-sm-6">
-   							 <label for="search">Enter Roll No.</label>
-    						<input type="number" class=" main form-control" placeholder="roll no">
-  								</div></div>
-						                
-                  <div class="card-body">
-         			        	<h1 class="lead">Subjects</h1>	
-			     	      <div class="form-group row">
-   			<label for="search" class="col-3">Maths</label>
-    		<input type="text" class=" col-4 form-control" id="mark-box-1" placeholder="mark">
-    	           </div>
-    	           <div class="form-group row">
-   			<label for="search" class="col-3">Scinnrce</label>
-    		<input type="text" class=" col-4 form-control" id="mark-box-2" placeholder="mark">
-    	           </div>
-            <center><input type="submit" name="submit" value="Add result " class="btn btn-outline-primary btn-lg"></center>
-        </div>
-						            </form>
-						        </div>
-						    </div>
-						</div>
-					</div>
->>>>>>> f11d45a8cc618635fb0b66371ddc74c93125ee39
+					   <div class="col-md-1"></div>
+					   <div class="col-md-10">
+						   <h1 class="card-title">Add Result</h1>
+						   <div class="card">
+							   <div class="card-body">
+								   <form class="" action="index.html" method="post">
+									   <div class="form-group row">
+										   <label for="exam_date" class="col-sm-2 col-form-label">Test date</label>
+										   <div class="col-md-4">
+											   <input type="date" class="form-control" name="exam_date" value="" required autofocus>
+										   </div>
+										   <label for="medium" class="col-sm-2 col-form-label">Medium</label>
+										   <div class="col-sm-4">
+											   <select class="form-control" name="medium">
+												   <option>Gujarati</option>
+												   <option>English(GSEB)</option>
+												   <option>English(CBSC)</option>
+											   </select>
+										   </div>
+									   </div>
+									   <div class="form-group row">
+										   <label for="std" class="col-sm-2 col-form-label">Std.</label>
+										  <div class="col-sm-4">
+											  <select class="form-control" name="std">
+												  <option>1</option>
+												  <option>2</option>
+												  <option>3</option>
+												  <option>4</option>
+												  <option>5</option>
+												  <option>6</option>
+												  <option>7</option>
+												  <option>8</option>
+												  <option>9</option>
+												  <option>10</option>
+											  </select>
+										  </div>
+									   </div>
+									   <div class="form-group row">
+										   <label for="roll_no" class="col-sm-2 col-form-label">Roll no.</label>
+										   <div class="col-sm-2">
+											   <input type="text" name="roll_no" class="form-control" value="" required>
+										   </div>
+									   </div>
+									   <div class="form-group row">
+										   <label for="student_name" class="col-sm-2 col-form-label">Student name</label>
+										   <div class="col-sm-6">
+											   <input type="text" class="form-control" name="student_name" value="" required>
+										   </div>
+									   </div>
+									   <div class="form-group row">
+										   <label class="col-sm-2 col-form-label header">Marks :</label>
+									   </div>
+									   <div class="form-group row">
+										   <label for="math" class="col-sm-2 col-form-label">Math</label>
+										   <div class="col-sm-3">
+											   <input type="text" name="math" class="form-control" value="">
+										   </div>
+									   </div>
+									   <div class="form-group row">
+										   <label for="science" class="col-sm-2 col-form-label">science</label>
+										   <div class="col-sm-3">
+											   <input type="text" name="science" class="form-control" value="">
+										   </div>
+									   </div>
+									   <div class="form-group row">
+										   <label for="ss" class="col-sm-2 col-form-label">Social science</label>
+										   <div class="col-sm-3">
+											   <input type="text" name="ss" class="form-control" value="">
+										   </div>
+									   </div>
+									   <center><input type="submit" name="submit" value="Submit" class="btn btn-outline-primary btn-lg"></center>
+								   </form>
+							   </div>
+						   </div>
+					   </div>
+				   </div>
 				</div>
 			</div>
 		</div>
 	</div>
-<<<<<<< HEAD
-=======
-
-
-
->>>>>>> f11d45a8cc618635fb0b66371ddc74c93125ee39
 </body>
 </html>
