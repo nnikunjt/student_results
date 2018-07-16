54641ef9063a1cc03cfec0e4560226625e06f959
kkishans <kishan7575t@gmail.com>
2018-07-16 22:45:04 +0530

    I change css of footer

--- admin/settings.php
+++ admin/settings.php
@@ -219,10 +219,9 @@ SESSION_START();
         </button>
       </div>
       <div class="modal-body">
-        First, Do this you have to Make your backup. If you Don't have made back up, make it.
+        
+        You have to make bachup Before resetting you tables.
         <br>
-        If you click ok' then you lose all saved data from your tables<br>
-        result, students<br>
 
         Did you really want to do that?<br>
 
